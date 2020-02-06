0210d3d4b4fc20034527c6b31ec93d8eb6e803f9
Šimon Kadlec <kadlesim@fel.cvut.cz>
2020-02-06 17:07:41 +0100

    first rest call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -8,6 +9,36 @@ import { TeamComponent } from './team/team.component';
 import { BlogComponent } from './blog/blog.component';
 import { ResultsComponent } from './results/results.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent
+  },
+  {
+    path: 'home',
+    component: HomepageComponent
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+  {
+    path: 'team',
+    component: TeamComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'results',
+    component: ResultsComponent
+  },
+  { path: '**', component: NotfoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,11 +47,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     EventsComponent,
     TeamComponent,
     BlogComponent,
-    ResultsComponent
+    ResultsComponent,
+    NotfoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Injectable, OnInit} from '@angular/core';
+import {DatePipe} from '@angular/common';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
+
+@Injectable({
+  providedIn: 'root'
+})
 export class EventsComponent implements OnInit {
 
-  constructor() { }
+  url = 'https://pd.tymy.cz/api/events?login=simon&password=605323511';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEvents().subscribe(data => console.log(data));
+  }
+
+  getEvents(): any {
+    // const endTime = new Date().getFullYear() + '' + new Date().getMonth() + '' + new Date().getDate();
+    const datePipe = new DatePipe('en-UK');
+    const endTime = datePipe.transform(Date.now(), 'yyyyMMdd');
+    console.log(endTime); /*Md5.hashStr("sem dat moje heslo")*/
+    // tslint:disable-next-line:max-line-length
+    this.url = 'https://pd.tymy.cz/api/events?login=simon&password=' + 'e0fcb7fc608c79e69fdc99ff7050aa72&filter=endTime>' + endTime +  '&order=startTime&limit=10';
+    // this.url = '../../assets/eventData.json'; //dummy data
+    console.log('log url >> ' + this.url); // endTime<' + endTime + '
+    // console.log();
+
+    return this.http.get(this.url);
   }
 
 }
