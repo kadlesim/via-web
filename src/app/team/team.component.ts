28e5cbd9c51f55922959c47bfe79cc97dce20c2f
Šimon Kadlec <kadlesim@fel.cvut.cz>
2020-02-10 19:47:26 +0100

    added rest calls

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DatePipe} from '@angular/common';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-team',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
-  constructor() { }
+  url = 'https://pd.tymy.cz/api/events?login=simon&password=605323511';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getAllPlayers().subscribe(data => console.log(data));
+  }
+
+  getAllPlayers(): any {
+    const datePipe = new DatePipe('en-UK');
+    const endTime = datePipe.transform(Date.now(), 'yyyyMMdd');
+    console.log(endTime); /*Md5.hashStr("sem dat moje heslo")*/
+    this.url = 'https://pd.tymy.cz/api/users?login=simon&password=' + 'e0fcb7fc608c79e69fdc99ff7050aa72';
+    console.log('log url >> ' + this.url);
+
+    return this.http.get(this.url);
+    // return this.http.get('http://127.0.0.1:10010/team'); //todo
   }
 
 }
